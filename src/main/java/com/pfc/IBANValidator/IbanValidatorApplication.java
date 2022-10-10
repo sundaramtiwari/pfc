61e773eb2246983b70aba82eb5ae5bb15867a1df
sundaramtiwari <sundaramtiwari@gmail.com>
2022-10-10 13:28:30 +0530

    Initial Commit

---
+++ src/main/java/com/pfc/IBANValidator/IbanValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.pfc.IBANValidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbanValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbanValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfc/IBANValidator/IbanValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfc.IBANValidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbanValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
